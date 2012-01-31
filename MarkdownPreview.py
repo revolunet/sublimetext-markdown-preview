5a9df57c7ee3e6600821d8ee0863ba05aacf2951
Julien Bouquillon <julien@bouquillon.com>
2012-01-31 11:38:30 +0100

    first commit

---
+++ MarkdownPreview.py
@@ -0,0 +1,19 @@
+import sublime, sublime_plugin
+import webbrowser
+import tempfile
+import markdown
+
+
+class MarkdownPreviewCommand(sublime_plugin.TextCommand):
+    """ preview file contents with python-markdown and your web browser"""
+    def run(self, edit):
+        region = sublime.Region(0, self.view.size())
+        contents = self.view.substr(region)
+        tmp_html = tempfile.NamedTemporaryFile(delete=False, suffix='.html')
+        html = markdown.markdown(contents)
+        encoding = self.view.encoding()
+        tmp_html.write('<meta charset="%s">' % self.view.encoding())
+        tmp_html.write('<link href="https://raw.github.com/revolunet/Markdown-CSS/master/markdown.css" rel="stylesheet"></link>')
+        tmp_html.write(html.encode(encoding))
+        tmp_html.close()
+        webbrowser.open_new_tab(tmp_html.name)
---
+++ README.md
@@ -0,0 +1,29 @@
+
+Sublime Text 2 MarkDown preview
+=====
+
+A simple ST2 plugin to help you preview your markdown files.
+
+Just preview the current md file in your web browser.
+
+**Installation :**
+
+ - `cmd+shift+P` then `install packages`
+ - select `MarkdownPreview'
+
+**Usage :**
+
+ - once installed :
+     - use `cmd+shift+P` then `MarkdownPreview` to launch a preview
+     - or bind some key in your user key binding
+
+**Uses :**
+
+ - [python-markdown][0] for markdown parsing
+ - [clownfart markown.css][1] for markdown styling
+
+ The code is avilable at github [https://github.com/revolunet/sublimetext-markdown-preview][2]
+
+ [0]: https://github.com/waylan/Python-Markdown
+ [1]: https://github.com/clownfart/Markdown-CSS
+ [2]: https://github.com/revolunet/sublimetext-markdown-preview
